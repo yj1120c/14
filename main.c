32c5dcea807b2e5f2221e310f40fac907446c0d6
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-12-03 15:47:14 +0900

    02

--- main.c
+++ main.c
@@ -3,20 +3,29 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+struct Book {
+	int number;
+	char title[20];
+};
+
 int main(int argc, char *argv[]) {
-	char *pc = NULL;
-	int i = 0;
+	struct Book *p;
 	
-	pc = (char*)malloc(100*sizeof(char));
-	if (pc == NULL) {
-	    printf("memory allocation error!\n");
-	    return -1;
-    }
-    
-    for (i=0;i<26;i++)
-    	pc[i] = 'a'+i;
+	p = (struct Book*)malloc(2*sizeof(struct Book)); //dynamic allocation of 2 Book structure instances
 	
-	pc[i] = 0;
-	printf("%s\n", pc);
-	free(pc);
+	if (p == NULL) {
+		printf("memory allocation error!\n");
+		return -1;
+	}
+	
+	p->number = 1;
+	strcpy(p->title, "C Programming");
+	
+	(p+1)->number = 2;
+	strcpy((p+1)->title, "Electronics");
+	
+	printf("%s %s\n", p->title, (p+1)->title);
+	
+	free(p);
+	return 0;
 }
