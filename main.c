6c16316a422ca0f840b6d063ffed765cf31116db
yj1120c <70882360+yj1120c@users.noreply.github.com>
2020-12-03 16:57:51 +0900

    04

--- main.c
+++ main.c
@@ -1,40 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "linkedList.h"
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-struct Book {
-	int number;
-	char title[20];
-};
-
 int main(int argc, char *argv[]) {
-	struct Book **bookshelf;
-	int i;
+	int n[5];
 	
-	bookshelf = (struct Book**)malloc(3*sizeof(struct Book*)); //3 pointer allocations
-	for (i=0;i<3;i++)
-	    bookshelf[i] = (struct Book*)malloc(10*sizeof(struct Book)); //10 allocation
-	/*
-	if (p == NULL) {
-	    printf("memory allocation error!\n");
-	    return -1;
-    }
-    */
-    
-    bookshelf[1][3].number = 5;
-    strcpy(bookshelf[1][3].title, "C++ Programming");
-    
-    (bookshelf[2]+4)->number = 3;
-    strcpy((bookshelf[2]+4)->title, "Communications Theory");
-    
-    printf("book (1,3) : %i, %s\n", (bookshelf[1]+3)->number, (bookshelf[1]+3)->title);
-    printf("book (2,4) : %i, %s\n", bookshelf[2][4].number, bookshelf[2][4].title);
-    
-    //free allocations
-    for (i=0;i<3;i++)
-        free(bookshelf[i]);
-    free(bookshelf);
-    
+	printf("input 5 integers :");
+	scanf("%d %d %d %d %d", n, n+1, n+2, n+3, n+4);
+	
+	insertDataToTail(n[0]);
+	insertDataToTail(n[1]);
+	insertDataToTail(n[2]);
+	insertDataToTail(n[3]);
+	insertDataToTail(n[4]);
+	
+	print_list();
+	print_node(2);
+	
 	return 0;
 }
